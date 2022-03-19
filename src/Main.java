fd418d26df732d4cf6fdd8f9574d9045f7a2d6c6
Anastasia <kaagernika@gmail.com>
2022-03-19 14:52:20 +0300

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello, %s!", name);
+    }
+}
